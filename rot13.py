8ea8dfa603d86f31289690fa5672e858e1acf6ba
ptoor <36647233+ptoor@users.noreply.github.com>
2019-10-14 22:12:22 +0200

    working rot13 decoder

---
+++ rot13.py
@@ -0,0 +1,5 @@
+import sys
+rot13 = str.maketrans( 
+    "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz", 
+    "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm")
+print(str.translate(sys.argv[1],rot13))
